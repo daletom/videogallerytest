c9b3b3d69d3267841ea7878ffc58cc4ab56d9973
Tom Dale <thomas.dale2@gmail.com>
2022-03-01 13:20:43 -0800

    Revert "add test page"
    This reverts commit 01469daf495cae90ac3f3952c274006b73921ce4.

--- src/pages/video.js
+++ src/pages/video.js
@@ -1,18 +1,8 @@
-import * as React from "react"
-//import { Link } from "gatsby"
+/*import React from "react"
 import VideoPlayerJS from './../components/player';
-//import { StaticImage } from "gatsby-plugin-image"
 
-import Layout from "../components/layout"
-import Seo from "../components/seo"
-
-const VideoPage = () => (
-  <Layout>
-    <Seo title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div 
+const VideoPage = ({ videoId }) => {
+  return <div 
         style={{
             width: '98vw', 
             height: '100vh', 
@@ -21,43 +11,31 @@ const VideoPage = () => (
             alignItems: 'center', 
             justifyContent: 'center'
      }}>
-        <div
-          style={{
-            margin: '10px'
-          }}
-        >
+        <div>
             <VideoPlayerJS
                 autoplay={false}
                 width={"640px"}
                 height={"360px"}
-                id={"HLSadvantages"}
-              />
-        </div>
-        <div
-          style={{
-            margin: '10px'
-          }}
-        >
-            <VideoPlayerJS
-                autoplay={false}
-                width={"640px"}
-                height={"360px"}
-                id={"gotopening"}
-              />
-        </div>
-        <div
-          style={{
-            margin: '10px'
-          }}
-        >
-            <VideoPlayerJS
-                autoplay={false}
-                width={"640px"}
-                height={"360px"}
-                id={"disney_zeek_dance2"}
+                id={videoId}
               />
         </div>
     </div>
+}
+
+export default VideoPage */
+
+import * as React from "react"
+import { Link } from "gatsby"
+
+import Layout from "../components/layout"
+import Seo from "../components/seo"
+
+const VideoPage = () => (
+  <Layout>
+    <Seo title="Page video" />
+    <h1>Hi from the video page</h1>
+    <p>It's not working now</p>
+    <Link to="/">Go back to the homepage</Link>
   </Layout>
 )
 
