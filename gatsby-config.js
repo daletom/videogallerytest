1b2a7bd0bb8b050f0b3826e0bb9183444637e0b0
Tom Dale <thomas.dale2@gmail.com>
2022-03-01 13:20:52 -0800

    Revert "add pages and update tailwind"
    This reverts commit 71f172e2aaf73e644a720a1c60c0b046215a97c5.

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,14 +1,13 @@
 module.exports = {
   siteMetadata: {
-    title: `Video Gatsby TailwindCSS Starter`,
-    description: `At one time or another, we have all seen the "building an image gallery with Gatsby tutorial." Well, it's time to up your game and build a video gallery tutorial!! In this workshop, you will learn how to create video gallery pages with Gatsby using all three of these modern video solutions — Strap in for an amazing workshop!`,
+    title: `imgix Video on Gatsby`,
+    description: `Test project to use imgix video HLS players on Gatsby.`,
     author: `@letsgoheadless`,
-  },
-  flags: {
-    THE_FLAG: false
+    siteUrl: `https://videogallerytestmaster.gatsbyjs.io/`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
+    `gatsby-plugin-image`,
     {
       resolve: `gatsby-source-filesystem`,
       options: {
@@ -16,9 +15,8 @@ module.exports = {
         path: `${__dirname}/src/images`,
       },
     },
-    `gatsby-plugin-image`,
-    `gatsby-plugin-sharp`,
     `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
@@ -26,27 +24,15 @@ module.exports = {
         short_name: `starter`,
         start_url: `/`,
         background_color: `#663399`,
-        theme_color: `#663399`,
+        // This will impact how browsers show your PWA/website
+        // https://css-tricks.com/meta-theme-color-and-trickery/
+        // theme_color: `#663399`,
         display: `minimal-ui`,
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
-    `gatsby-plugin-offline`,
-    {
-      resolve: `gatsby-plugin-postcss`,
-      options: {
-        postCssPlugins: [require("tailwindcss"), require("autoprefixer")]
-      }
-    },
-    {
-      resolve: `gatsby-plugin-purgecss`,
-      options: {
-        printRejected: false,
-        develop: false,
-        tailwind: true,
-      },
-    }
+    // `gatsby-plugin-offline`,
   ],
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,68 +1,62 @@
-import React from "react"
-import { Link } from "gatsby"
+import * as React from "react"
+//import { Link } from "gatsby"
+import VideoPlayerJS from './../components/player';
+//import { StaticImage } from "gatsby-plugin-image"
+
 import Layout from "../components/layout"
-import SEO from "../components/seo"
+import Seo from "../components/seo"
 
 const IndexPage = () => (
   <Layout>
-    <SEO title="Home" />
-    <div className="mx-auto py-12 px-4 max-w-7xl sm:px-6 lg:px-8 lg:py-24">
-      <div className="space-y-12">
-        <div className="space-y-5 sm:space-y-4 md:max-w-xl lg:max-w-3xl xl:max-w-none">
-          <h1 className="text-3xl font-extrabold tracking-tight sm:text-4xl">Building Video Galleries with Gatsby</h1>
-          <p className="text-xl text-gray-500">At one time or another, we have all seen the "building an image gallery with Gatsby tutorial." Well, it's time to up your game and build a video gallery tutorial!! In this workshop, you will learn how to create video gallery pages with Gatsby using all three of these modern video solutions — Strap in for an amazing workshop!</p>
+    <Seo title="Home" />
+    <h1>Hi people</h1>
+    <p>Welcome to your new Gatsby site.</p>
+    <p>Now go build something great.</p>
+    <div 
+        style={{
+            width: '98vw', 
+            height: '100vh', 
+            display: 'flex',
+            flexDirection: 'column', 
+            alignItems: 'center', 
+            justifyContent: 'center'
+     }}>
+        <div
+          style={{
+            margin: '10px'
+          }}
+        >
+            <VideoPlayerJS
+                autoplay={false}
+                width={"640px"}
+                height={"360px"}
+                id={"HLSadvantages"}
+              />
         </div>
-        <ul
-            role="list"
-            className="space-y-12 sm:grid sm:grid-cols-2 sm:gap-x-6 sm:gap-y-12 sm:space-y-0 lg:grid-cols-3 lg:gap-x-8"
-          >
-            <li>
-            <Link to="/youtube/">
-              <div className="space-y-4">
-                  <div className="aspect-w-3 aspect-h-2">
-                    <img className="object-cover shadow-lg rounded-lg" src="https://tom.imgix.net/magento1.png?auto=format,compress&w=800" alt="" />
-                  </div>
-                <div className="space-y-2">
-                  <div className="text-lg leading-6 font-medium space-y-1">
-                    <h3>Youtube</h3>
-                    <p className="text-indigo-600">Simple way to use an iframe to embed Youtube Videos on your Gatsby site.</p>
-                  </div>
-                </div>
-              </div>
-              </Link>
-            </li>
-            <li>
-              <Link to="/mp4/">
-              <div className="space-y-4">
-                  <div className="aspect-w-3 aspect-h-2">
-                    <img className="object-cover shadow-lg rounded-lg" src="https://tom.imgix.net/magento2.png?auto=format,compress&w=800" alt="" />
-                  </div>
-                <div className="space-y-2">
-                  <div className="text-lg leading-6 font-medium space-y-1">
-                    <h3>MP4</h3>
-                    <p className="text-indigo-600">Use MP4, a tried and true video format to host your own video urls.</p>
-                  </div>
-                </div>
-              </div>
-              </Link>
-            </li>
-            <li>
-            <Link to="/hls/">
-              <div className="space-y-4">
-                  <div className="aspect-w-3 aspect-h-2">
-                    <img className="object-cover shadow-lg rounded-lg" src="https://tom.imgix.net/magento3.png?auto=format,compress&w=800" alt="" />
-                  </div>
-                <div className="space-y-2">
-                  <div className="text-lg leading-6 font-medium space-y-1">
-                    <h3>HLS</h3>
-                    <p className="text-indigo-600">A newer format that can intelligently adjust bitrate to ensure your video never stops playing.</p>
-                  </div>
-                </div>
-              </div>
-              </Link>
-            </li>
-        </ul>
-      </div>
+        <div
+          style={{
+            margin: '10px'
+          }}
+        >
+            <VideoPlayerJS
+                autoplay={false}
+                width={"640px"}
+                height={"360px"}
+                id={"gotopening"}
+              />
+        </div>
+        <div
+          style={{
+            margin: '10px'
+          }}
+        >
+            <VideoPlayerJS
+                autoplay={false}
+                width={"640px"}
+                height={"360px"}
+                id={"disney_zeek_dance2"}
+              />
+        </div>
     </div>
   </Layout>
 )
